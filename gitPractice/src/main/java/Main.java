8f1ba82e8ac0bbd04c57f390ec49e9bf99f85f53
ivan krasiuk <ivankrasiuk@gmail.com>
2018-05-20 19:36:06 +0300

    second squash of commits

--- gitPractice/src/main/java/Main.java
+++ gitPractice/src/main/java/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {  // Master Brunch method
 
+        // Third commite of brunch addClass
+        Print.print("Third commit");
+
+        //second commite after squash
+        Print.plus(4, 7);
     }
 }
---
+++ gitPractice/src/main/java/Print.java
@@ -0,0 +1,15 @@
+public class Print {
+
+    // first commit, add empty class of brunch addClass
+    public static void print(String string) {
+        System.out.println(string);
+        //secont commit add method print of brunch addClass
+    }
+
+    //first commit after squash
+    //add plus method
+    public static void plus(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
+    }
+}
